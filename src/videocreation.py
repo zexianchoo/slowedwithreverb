aa7a5f8fc62fd130449edc2e9262ba3439f39468
Ze Xian Choo <99162756+zexianchoo@users.noreply.github.com>
2024-06-17 07:42:00 +0800

    fix: added code removed by refactor

---
+++ src/videocreation.py
@@ -0,0 +1,160 @@
+import moviepy.editor as mp
+from  moviepy.editor import ColorClip,TextClip,AudioFileClip
+import requests
+import os
+from constants import *
+import random
+
+VIDEO_PATH= os.path.join(MEDIA_PATH, VIDEO_BASENAME)
+GIF_PATH= os.path.join(MEDIA_PATH, GIF_BASENAME)
+os.makedirs(VIDEO_PATH, exist_ok=True)
+os.makedirs(GIF_PATH, exist_ok=True)
+
+GIPHY_ENDPOINT="https://api.giphy.com/v1/gifs/search"
+
+"""
+Download new gifs to keep the gif stockpile fresh, adds key value pair to redis
+"""
+def loadRedisWithGIFS(redis_server, api_key, endpoint=GIPHY_ENDPOINT, search_term=GIF_SEARCH_TERM, limit=25):
+    params = {
+        'q': search_term,
+        'api_key': api_key,
+        'rating': 'pg-13', #general,
+        'limit': limit
+    }
+    res = requests.get(url=endpoint, params=params)
+    retcode = res.json()['meta']['status']
+
+    # download the gif    
+    for gif in res.json()['data']:
+        gif_id = "gif:" + gif['id']
+        
+        # gets the original version's direct url.
+        gif_url = gif['images']['original']['url']
+        
+        # only add new gifs to the redis server
+        if not redis_server.exists(gif_id):
+            redis_server.hset(gif_id, "gif_url", gif_url)
+            redis_server.hset(gif_id, "visited", 0)
+
+    return retcode, gif_id, gif_url
+
+def getNotVisitedHeper(redis_server):
+    cursor = 0
+    while True: 
+        cursor, keys = redis_server.scan(cursor=cursor, match='gif:*', count=100)
+        for key in keys:
+            visited = redis_server.hget(key, 'visited')
+            if visited == b'0':
+                # found key that has not been visited
+                value = redis_server.hgetall(key)
+                
+                return key, value[b'gif_url'].decode('utf-8')
+        if cursor == 0:
+            break 
+    return "NULL"
+
+"""
+Searches redis for new gifs that have visited flag set to 0, returns the gif path from the url of that gif
+if we cant find anything, then we will have to call loadRedisWithGIFS again, or with another search term
+"""
+def getNotVisited(redis_server, api_key):
+    
+    res = getNotVisitedHeper(redis_server)
+    if res != "NULL":
+        return res
+    
+    # did not find, try to search again:
+    loadRedisWithGIFS(redis_server, api_key)
+    res = getNotVisitedHeper(redis_server)
+    if res != "NULL":
+        return res
+        
+    # still didnt find!!! we will use trending.
+    loadRedisWithGIFS(api_key, search_term="trending", limit=5)
+    res = getNotVisitedHeper(redis_server)
+    return res
+
+
+"""
+Helper to download the gif from the url, and save to path
+returns output path
+"""
+def downloadGIF(gif_id, gif_url):
+    print("Getting Request for GIF...")
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(gif_url, headers=headers)
+    save_path = os.path.join(GIF_PATH, gif_id.decode('utf-8')[4:] + ".gif")
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Open the file and write the content of the response
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        print(f'GIF saved to {save_path}')
+    else:
+        print(f'Failed to download GIF. Status code: {response.status_code}')
+
+    return save_path, gif_url
+
+"""
+downloads a fresh gif and returns the output path
+"""
+def getNewGIF(redis_server, api_key):
+    
+    # get a fresh gif
+    key, gif_url = getNotVisited(redis_server, api_key)
+    
+    output_path, gif_url = downloadGIF(key, gif_url)
+    
+    # set visited to true
+    redis_server.hset(key, "visited", 1)
+    return output_path, gif_url
+     
+def createVideoFromGIF(audio_path, gif_path, yt_vidname):
+    
+    audio_track = AudioFileClip(audio_path)
+
+    #dark background
+    hdres = [1280, 720]
+    
+    mellow_colors = [
+        [240, 230, 140],  # Mellow Yellow
+        [255, 165, 0],    # Mellow Orange
+        [152, 251, 152],  # Mellow Green
+        [173, 216, 230],  # Mellow Blue
+        [204, 153, 255],  # Mellow Purple
+        [255, 182, 193],  # Mellow Pink
+        [255, 218, 185],  # Mellow Peach
+        [230, 230, 250],  # Mellow Lavender
+        [189, 252, 201],  # Mellow Mint
+        [200, 162, 200],  # Mellow Lilac
+        [135, 206, 235],  # Mellow Sky Blue
+        [244, 164, 96]    # Mellow Sand
+    ]
+    
+    bg_clip = ColorClip(size = hdres, color = random.choice(mellow_colors)).set_duration(audio_track.duration)
+
+    #selected GIF
+    animated_gif = (mp.VideoFileClip(gif_path)
+            .resize(width=int(0.75 * bg_clip.size[0]), height=int(0.75 * bg_clip.size[1]))
+            .loop()
+            .set_duration(audio_track.duration)
+            .set_pos(("center","center")))
+
+    #custom made formula to set words below the animated GIF
+    var_y = 0.5 * (bg_clip.size[1] - animated_gif.size[1])
+    new_y = animated_gif.size[1] + 1.25 * var_y
+    new_x = "center" 
+
+    title_clip = TextClip(txt=yt_vidname, fontsize=20, font='Palatino-Italic', color='white').set_duration(audio_track.duration)
+    title = title_clip.loop().set_pos((new_x,new_y))
+
+    setaudioclip = animated_gif.set_audio(audio_track)
+
+    file_basename = yt_vidname.replace(' ', '')
+    final = mp.CompositeVideoClip([bg_clip, setaudioclip, title])
+    output_path = os.path.join(VIDEO_PATH, file_basename) + ".mp4"
+    final.write_videofile(output_path, threads=1, codec="libx264")
+    
+    return output_path
---
+++ src/ytdl.py
@@ -0,0 +1,36 @@
+from requests import get
+from yt_dlp import YoutubeDL
+import os
+from constants import *
+
+# create the out path to store the audio files
+OUT_PATH= os.path.join(MEDIA_PATH, AUDIO_BASENAME)
+os.makedirs(OUT_PATH, exist_ok=True)
+"""
+Uses yt-dlp to download audio files into the outpath.
+"""
+def downloadAudio(track):
+    final_filename = None
+    
+    artist = track['artists'][0]['name'].replace(' ', '')
+    output_filename = "{0}/{1}_{2}".format(OUT_PATH, artist, track['name']).replace(' ', '')
+    search_query = "{0} {1} lyrics".format(track['name'], artist)
+    print(output_filename)
+    yt_opts = {
+        'noplaylist': True,
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+        }],
+        "outtmpl": output_filename,
+        'prefer_ffmpeg': True,
+        'verbose': False,
+        'quiet': True, 
+        'noprogress': True
+    }
+    with YoutubeDL(yt_opts) as ydl:
+        retcode = ydl.download(f"ytsearch:{search_query}" )
+        print("output_filename: ", output_filename)
+    # return the return code 
+    return retcode, output_filename + ".wav"
